dafb99ede89ff9d7c13e6975ed9300a4fd33bb48
sunmin98 <miniks130@naver.com>
2022-06-13 12:02:47 +0900

    6/13

---
+++ 06_13_2581.py
@@ -0,0 +1,33 @@
+a=int(input())
+b=int(input())
+list = []
+
+if a==1 and b==2:   #반례처리
+    list.append(b)
+
+
+for i in range(a,b):
+    cnt=0
+    if(i == 1):
+        continue
+    for j in range(2, i+1):
+        if(i % j == 0):
+            cnt += 1
+    if(cnt == 1):
+        list.append(i)
+
+if a==b:    #값이 같을떄
+    cnt=0
+    for j in range(2, a + 1):
+        if (a % j == 0):
+            cnt += 1
+    if (cnt == 1):
+        list.append(a)
+
+
+if len(list)==0:
+    print(-1)
+    exit()
+
+print(sum(list))
+print(min(list))

1b94d4e342b914a1a85587847879297880b81393
sunmin98 <miniks130@naver.com>
2022-06-14 11:32:03 +0900

    6/14

--- 06_13_2581.py
+++ 06_13_2581.py
@@ -5,17 +5,6 @@ list = []
 if a==1 and b==2:   #반례처리
     list.append(b)
 
-
-for i in range(a,b):
-    cnt=0
-    if(i == 1):
-        continue
-    for j in range(2, i+1):
-        if(i % j == 0):
-            cnt += 1
-    if(cnt == 1):
-        list.append(i)
-
 if a==b:    #값이 같을떄
     cnt=0
     for j in range(2, a + 1):
@@ -23,6 +12,19 @@ if a==b:    #값이 같을떄
             cnt += 1
     if (cnt == 1):
         list.append(a)
+else:
+    for i in range(a,b+1):
+        cnt=0
+        if(i == 1):
+            continue
+        for j in range(2, i+1):
+            if(i % j == 0):
+                cnt += 1
+        if(cnt == 1):
+            list.append(i)
+
+
+
 
 
 if len(list)==0:

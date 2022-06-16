b6c6d5b8032d898222f283757c330b13886e6477
sunmin98 <miniks130@naver.com>
2022-06-16 20:25:44 +0900

    6/16

--- 06_15_1929.py
+++ 06_15_1929.py
@@ -1,17 +1,24 @@
 a, b = map(int, input().split())
+list=[]
 
-for num in range(a, b+1):
-    pan=0
+for i in range(a,b+1):
+    list.append(i)
 
-    for i in range(2,num+1):
-        if num % i == 0:  #나누어 떨어진다는뜻
-            pan=1
-        # if num==a and num % a ==0:
-        #     pan=0
+for i in list: #i가 나누어짐 당하는애
+    if i % 2 == 0 : list.remove(i)
+    if i % 3 == 0 : list.remove(i)
+    if i % 5 == 0 : list.remove(i)
+    if i % 7 == 0 : list.remove(i)
 
-    if pan == 0 :
-        print(num)
+print(list)
 
-
-
-
+# for i in range(a,b+1):
+#     #i 는 차례로 나오는 주인공
+#     cnt = 0 #소수인지아닌지
+#     if(i == 1): # 1은 소수가 아니기 때문에 건너띔
+#         continue
+#     for j in range(2, i+1): #j는 소수 나누는에
+#         if(i % j == 0): #나누어진다는거는 소수
+#             cnt += 1  #소수 마킹
+#     if(cnt == 1): #소수라는거
+#         print(i)

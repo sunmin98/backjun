f378fef9200e31dc2457eeaab604518e32c0cbff
sunmin98 <miniks130@naver.com>
2022-09-18 15:38:18 +0900

    9/18

--- 2022/09/14_2477.py
+++ 2022/09/14_2477.py
@@ -1,9 +1,29 @@
+a = int(input())
+
+max_height = 0  # 가장 긴 높이를 저장할 변수 3,4
+max_width = 0  # 가장 긴 가로길이를 저장할 변수 1,2
+max_width_index = []  # 가장 긴 가로길이의 인덱스를 저장할 변수 1 2
+max_height_index = []  # 가장 긴 높이의 인덱스를 저장할 변수 3 4
+
+lists = []  # 변의 정보들을 저장할 리스트
+for i in range(6):
+    lists.append(list(map(int, input().split())))
+
+for i in range(6):
+    if lists[i][0]==3 or lists[i][0]==4 : max_width_index.append(lists[i][1])
+
+max_width=max(max_width_index)
+print(max_width)
+
+for i in range(6):
+    if lists[i][0]==1 or lists[i][0]==2 : max_height_index.append(lists[i][1])
 
 
-import math
-a=int(input())
+print(max_width_index)
+print(max_height_index)
+print(lists)
 
-print(a**2*math.pi)
-print(a**2*2)
+
+
 
 
